7cfa46b9988dc79999657d490f7d83bd51591aed
UserForBruteer <114910606+UserForBruteer@users.noreply.github.com>
2024-03-18 23:12:53 +0600

    DELTA UPDATE

--- just-button.py
+++ just-button.py
@@ -2,14 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 
 class Plugin:
-    def execute(self, **kwargs):
-        self.root = kwargs.get('root')
-        self.tab_control = kwargs.get('tab_control')
-        self.add_button()
-
-    def add_button(self):
-        new_button = ttk.Button(self.root, text="Plugin Button", command=self.on_button_click)
-        new_button.pack(side=tk.LEFT, padx=5)
+    def __init__(self, api):
+        self.api = api
+        api.create_plugin_button("Exemple", self.on_button_click)
 
     def on_button_click(self):
         print("Plugin Button Clicked!")

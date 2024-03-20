9588f28a3fca63c701e85294c5042001c65d9b12
UserForBruteer <114910606+UserForBruteer@users.noreply.github.com>
2024-03-20 16:16:46 +0600

    button // minimal version: 1.3.1
    api version: 1

--- just-button.py
+++ just-button.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 class Plugin:
     def __init__(self, api):
         self.api = api
-        api.create_plugin_button("Exemple", self.on_button_click)
+        api.create_plugin_button("Button", self.on_button_click, button_id="plugin_button")
 
     def on_button_click(self):
         print("Plugin Button Clicked!")
